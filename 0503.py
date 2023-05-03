a221b3b1650f9af9c41c86b2a8688d1c0d669464
MinChaeY <alscodud86@gmail.com>
2023-05-03 16:43:34 +0900

    일단 끝

--- 0503.py
+++ 0503.py
@@ -4,8 +4,8 @@
 함수이름 - addthree
 기능 3을 더함
 input : 숫자 1개ㅣ
-output 숫자 한개\\,input 숫자에다가 3을 더한 것을 내보냄
-'''
+output 숫자 한개,input 숫자에다가 3을 더한 것을 내보냄
+
 def addthree(num):
     return num+3
 
@@ -43,11 +43,11 @@ list1 =[1,2,3,4,5,6,7,8,9,10]
 list2=[10,11,12,13,14,15,16,17]
 print(sum2(*list1))
 print(sum2(*list2))
-
+'''
 '''key value pair
 cafe function
 메뉴=가격
-출력해주는 function을 만들자'''
+출력해주는 function을 만들자
 
 def cafe(**keyvalue):
     #수행문 menu와 가격을 출력하라.
@@ -87,6 +87,51 @@ print((lambda num5,num6: num5*num6)(d,f))
 def mul2(d,f):
     return d*f
 
-print(mul2(d,f))
+print(mul2(d,f))'''
+'''
+#map, filter
+map(function,list)
+map(function,[1,2,3,4,5])
+[function(1),function(2),function(3),...,function(5)]
 
+print(list(map(addthree,[10,20,30,40,50])))
 
+print(list(map(lambda x: x+3,[10,20,30,40,50])))
+'''
+'''
+[addthree(10),addthree(20),addthree(30),addthree(40),addthree(50)]
+[13,23,33,43,53]
+input -function,list
+output list하나
+
+list3=[1,2,3,4,5,6]
+#5배를 하고 10을 더해서 결과를 list 로 출력
+def sumul(g):
+    return g*5+10
+print(list(map(sumul,list3)))
+
+#lambda로
+print(list(map(lambda x: x*5+10,list3)))
+'''
+#두개의 리스트를 하나씩 뽑아서 두개를 더해 하나의 리스트를 만들어라
+lst10 =[10,20,30,40,50]
+lst11=[1,2,3,4,5]
+
+def mysum(n1,n2):
+    return n1+n2
+print(list(map(mysum,lst10,lst11)))
+
+print(list(map(lambda n1,n2:n1+n2,lst10,lst11)))
+
+#filter
+#조건을 만족하면 결과에 포함, 만족하지 않으면 포함x
+#map과 마찬가지로 리스트를 입력받아 그걸 매번 체크하여 포함시킴
+#filter(function,list)
+def positive(n):
+    if n > 0:
+        return True
+    else:
+        return False
+print(list(filter(positive,[10,2,-3,5,-9])))
+
+print(list(filter(lambda x: x>0,[10,2,-3,5,-9])))
